d7c424667739848b0c779a414db9366d77b18d2b
burrozd <95920902+burrozd@users.noreply.github.com>
2023-03-19 18:02:27 +0100

    tests

--- algorithm.test.js
+++ algorithm.test.js
@@ -1,8 +1,22 @@
-import { describe, expect, it } from "@jest/globals";
+import { describe, expect, it, test } from "@jest/globals";
 import checkLetterMatch from "./algorithm";
 
 describe("checkLetterMattch(word1,word2)", () => {
     it("makes all letters upperCase()");
     const result = checkLetterMatch("House");
     expect(result).toBe("HOUSE");
+});
+
+test("test if word1 contains five letters", () => {
+    const word1Length = "House";
+    expect(word1Length.length).toBe(5);
+});
+
+test("word1 has five letters", () => {
+    expect(word1).toHaveLength(5);
+});
+
+test("word2 is uppercase", () => {
+    checkLetterMatch(word1, word2);
+    expect(word2).toBe("GAMES");
 });
